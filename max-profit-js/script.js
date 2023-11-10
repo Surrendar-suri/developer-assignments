f266abd48e3425222b2fa839acf9f2405d10a40c
Surendar <surendar.s@ippopay.com>
2023-11-10 18:44:30 +0530

    Initial Commit

---
+++ max-profit-js/script.js
@@ -0,0 +1,35 @@
+function maxProfit(n) {
+    // Initialize counts for each type of property
+    let theaters = 0;
+    let pubs = 0;
+    let parks = 0;
+  
+    // Calculate the maximum number of each property based on the time units
+    while (n >= 10) {
+      parks += 1;
+      n -= 10;
+    }
+  
+    while (n >= 5) {
+      theaters += 1;
+      n -= 5;
+    }
+  
+    while (n >= 4) {
+      pubs += 1;
+      n -= 4;
+    }
+  
+    // Calculate earnings
+    const earnings = theaters * 1500 + pubs * 1000 + parks * 3000;
+  
+    const result = `T:${theaters}P:${pubs}C:${parks}`;
+  
+    return [result, earnings];
+  }
+  
+  // Test cases
+  console.log(maxProfit(7));   // Output: ['T:1P:0C:0', 1500]
+  console.log(maxProfit(8));   // Output: ['T:1P:0C:0', 1500]
+  console.log(maxProfit(13));  // Output: ['T:2P:0C:0', 3000]
+  
